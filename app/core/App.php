bfd3919de2d7044c28e909ac27d52832a7e05b08
Wijdan Muhammad R <42838967+youudan@users.noreply.github.com>
2021-09-30 00:28:45 +0700

    pertemuan 4, video 3 - routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,17 @@ class App
 {
   public function __construct()
   {
-    echo 'OK!';
+    $url = $this->parseUrl();
+    var_dump($url);
+  }
+
+  public function parseUrl()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
