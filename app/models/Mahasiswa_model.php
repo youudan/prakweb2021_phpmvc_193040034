a5e6dfb532c4edebc939c4272707c2cd1d78fa36
Wijdan Muhammad R <42838967+youudan@users.noreply.github.com>
2021-09-30 13:08:49 +0700

    pertemuan 4, menambahkan file database phpmvc

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,35 +1,23 @@
 <?php
 class Mahasiswa_model
 {
-  private $mhs = [
-    [
-      "nama"    => "Wijdan MUhammad Ridwanulloh",
-      "nrp"     => "193040034",
-      "email"   => "193040034.wijdan@mail.unpas.ac.id",
-      "jurusan" => "Teknik Informatika"
-    ],
-    [
-      "nama"    => "Putra Hanif Ammarta",
-      "nrp"     => "193040001",
-      "email"   => "193040001.putra@mail.unpas.ac.id",
-      "jurusan" => "Teknik Informatika"
-    ],
-    [
-      "nama"    => "Agung Gumelar",
-      "nrp"     => "193040008",
-      "email"   => "193040008.agung@mail.unpas.ac.id",
-      "jurusan" => "Teknik Informatika"
-    ],
-    [
-      "nama"    => "Aryogi Aziz",
-      "nrp"     => "1930400223",
-      "email"   => "193040023.aryogi@mail.unpas.ac.id",
-      "jurusan" => "Teknik Informatika"
-    ]
-  ];
+  private $dbh; // database
+  private $stmt;
 
+  public function __construct()
+  {
+    // data source name
+    $dsn = 'mysql:host=localhost;dbname=phpmvc';
+    try {
+      $this->dbh = new PDO($dsn, 'root', '');
+    } catch (PDOException $e) {
+      die($e->getMessage());
+    }
+  }
   public function getAllMahasiswa()
   {
-    return $this->mhs;
+    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+    $this->stmt->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 }
---
+++ phpmvc.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2021 at 08:05 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpmvc`
+--
+CREATE DATABASE IF NOT EXISTS `phpmvc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `phpmvc`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nrp` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`) VALUES
+(1, 'Wijdan Muhammad Ridwanulloh', '193040034', '193040034.wijdan@mail.unpas.ac.id', 'Teknik Informatika'),
+(2, 'Putra Hanif Ammarta', '193040001', '193040001.putra@mail.unpas.ac.id', 'Teknik Informatika'),
+(3, 'Agung Gumelar', '193040008', '193040008.agung@mail.unpas.ac.id', 'Teknik Informatika'),
+(4, 'Aryogi Aziz', '193040023', '193040023.aryogi@mail.unpas.ac.id', 'Teknik Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
